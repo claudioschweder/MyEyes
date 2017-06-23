4a90dd2116a0bc373038658898697e0a5363a47d
eduardo.voltolini <eduardoftl@hotmail.com>
2017-06-23 19:25:30 -0300

    add

--- MyEyes/MyEyes_Interface/MainActivity.cs
+++ MyEyes/MyEyes_Interface/MainActivity.cs
@@ -12,26 +12,58 @@ namespace MyEyes
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Main);
 
-            Button Confirmar = FindViewById<Button>(Resource.Id.Capturar);
-            Confirmar.Click += delegate { Teste(); };
+            // Criação de botões
+            Button BtnCapturar = FindViewById<Button>(Resource.Id.Capturar);
+            Button BtnRepetir = FindViewById<Button>(Resource.Id.Repetir);
+            Button BtnVoltar = FindViewById<Button>(Resource.Id.Voltar);
 
-            Button Repetir = FindViewById<Button>(Resource.Id.Repetir);
-            Repetir.Click += delegate { Teste(); };
+            // Definição dos metodos
+            BtnCapturar.Click += delegate { Capturar(); };
+            BtnRepetir.Click += delegate { Repetir(); };
+            BtnVoltar.Click += delegate { Voltar(); };
         }
 
-        public void Confirmar()
+        public void Capturar()
+        {
+            //define o alerta para executar a tarefa
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog alerta = builder.Create();
+            //Define o Titulo
+            alerta.SetTitle("Simulador sem câmera");
+            alerta.SetIcon(Android.Resource.Drawable.IcDialogAlert);
+            alerta.SetMessage("Por estar utilizando um emulador, não tem câmera.");
+            alerta.SetButton("OK", (s, ev) =>
+            {
+                Toast.MakeText(this, "Legal, vamos continuar... !", ToastLength.Short).Show();
+            });
+            alerta.Show();
+        }
+
+        public void Repetir()
         {
 
         }
 
-        public void Confirmar()
+        public void Voltar()
         {
-
-        }
-
-        public void Confirmar()
-        {
-
+            //define o alerta para executar a tarefa
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+            alerta.SetTitle("Sair");
+            alerta.SetIcon(Android.Resource.Drawable.IcDialogAlert);
+            alerta.SetMessage("Deseja realmente sair");
+            //define o botão positivo
+            alerta.SetPositiveButton("Ok", (senderAlert, args) =>
+            {
+                Toast.MakeText(this, "Saindo...", ToastLength.Short).Show();
+            });
+            //define o botão negativo
+            alerta.SetNegativeButton("Cancelar", (senderAlert, args) =>
+            {
+                Toast.MakeText(this, "Cancelado !", ToastLength.Short).Show();
+            });
+            //cria o alerta e exibe
+            Dialog dialog = alerta.Create();
+            dialog.Show();
         }
     }
 }
